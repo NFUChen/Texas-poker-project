d83d01a8d22a89216240d317941a6d08f8867dad
William Chen <10979111@gm.nfu.edu.tw>
2022-02-17 23:48:05 +0800

    Refactor .best_rank() method, whichc becomes a bulky function over time. With some helper functions implemented, .best_rank() method becomes clean and organized

--- poker/hand.py
+++ poker/hand.py
@@ -1,43 +1,78 @@
 
-from typing import List
+from typing import Callable, List, Tuple
 from poker.card import Card
-
+    
 
 class Hand:
     def __init__(self, cards:List[Card]) -> None:
         self.cards = cards
 
+    
+
+    @property
+    def _rank_validations_from_best_to_worst(self) -> Tuple[Tuple[str, Callable]]:
+        '''
+        Helper property of .best_rank() method, 
+        which the returned tuple (which will be unpacked as well) will be iterated over, 
+        '''
+        return (
+        ("Three of A Kind", self._three_of_a_kind),
+        ("Two Pair", self._two_pair),
+        ("Pair", self._pair),
+        ("High Card",self._high_card),
+    )
+
+
     def best_rank(self) -> str:
-        
+        '''
+        Figures out what is the best rank 
+        using _rank_validations_from_best_to_worst helper property
+        '''
 
-
+        for rank_name_with_validator_tuple in self._rank_validations_from_best_to_worst:
+            rank_name, validator_func = rank_name_with_validator_tuple
+            if validator_func():
+                return rank_name
         
-        
-
-        ranks_with_three_of_a_kind = self._ranks_with_target_count(3)
-        if len(ranks_with_three_of_a_kind) == 1:
-            return "Three of A Kind"
-        
-        ranks_with_pairs = self._ranks_with_target_count(2)
-
-        if len(ranks_with_pairs) == 1:
-            return "Pair"
-        if len(ranks_with_pairs) == 2:
-            return "Two Pair"
             
 
 
 
 
+    
+    
+    def _three_of_a_kind(self) -> bool:
+        '''
+        Test if current hand has three of a a kind.
+        '''
+        ranks_with_three_of_a_kind = self._ranks_with_target_count(3)
+        return len(ranks_with_three_of_a_kind) == 1
 
+    def _two_pair(self) -> bool:
+        '''
+        Test if current hand has two pair.
+        '''
+        ranks_with_pairs = self._ranks_with_target_count(2)
+        return len(ranks_with_pairs) == 2
 
-        return "High Card"
+    def _pair(self) -> bool:
+        '''
+        Test if current hand has pair.
+        '''
+        ranks_with_pair = self._ranks_with_target_count(2)
+        return len(ranks_with_pair) == 1
+
+    def _high_card(self) -> bool:
+        '''
+        Test if current hand has high card.
+        '''
+        return True
+
     
     def _ranks_with_target_count(self, target_count:int) -> dict:
         '''
         coupled with _card_rank_counts() method
         '''
-
         return {
             rank: rank_count 
             for rank, rank_count in self._card_rank_counts.items() 
@@ -59,6 +94,7 @@ class Hand:
         return card_rank_count
 
     
+
     @property
     def _card_suit_counts(self) -> dict:
         card_suit_count = {}

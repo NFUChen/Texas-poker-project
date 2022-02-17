7dfa9f9e01a3988b55f823665d7060f185a79fa0
William Chen <10979111@gm.nfu.edu.tw>
2022-02-17 16:56:22 +0800

    develop Card class in card.py

---
+++ tests/test_card.py
@@ -0,0 +1,14 @@
+import unittest
+from poker.card import Card
+
+
+class CardTest(unittest.TestCase):
+    def test_setup(self):
+        Card()
+
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main()

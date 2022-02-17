58578d81668ca34ba16b867da548c84493f52700
William Chen <10979111@gm.nfu.edu.tw>
2022-02-17 23:17:57 +0800

    finish best_rank() method, it can figures out 'three of A Kind' as best rank

--- poker/hand.py
+++ poker/hand.py
@@ -11,9 +11,14 @@ class Hand:
         
 
 
-        ranks_with_pairs = {rank: rank_count 
-                            for rank, rank_count in self._card_rank_counts.items() 
-                            if rank_count == 2}
+        
+        
+
+        ranks_with_three_of_a_kind = self._ranks_with_target_count(3)
+        if len(ranks_with_three_of_a_kind) == 1:
+            return "Three of A Kind"
+        
+        ranks_with_pairs = self._ranks_with_target_count(2)
 
         if len(ranks_with_pairs) == 1:
             return "Pair"
@@ -28,8 +33,24 @@ class Hand:
 
         return "High Card"
     
+    def _ranks_with_target_count(self, target_count:int) -> dict:
+        '''
+        coupled with _card_rank_counts() method
+        '''
+
+        return {
+            rank: rank_count 
+            for rank, rank_count in self._card_rank_counts.items() 
+            if rank_count == target_count
+               }
+
+
     @property
     def _card_rank_counts(self) -> dict:
+        '''
+        coupled with _ranks_with_target_count() method
+        '''
+
         card_rank_count = {}
         for card in self.cards:
             card_rank_count.setdefault(card.rank, 0) # if the key already exists, do nothing

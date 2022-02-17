5b3a9bf78f609bea7aafbfba4b4f1c8862c406df
William Chen <10979111@gm.nfu.edu.tw>
2022-02-17 21:33:09 +0800

    finish Deck class in deck.py, begin to write a function resposible for generating 52 standard cards in Card class in card.py

--- poker/card.py
+++ poker/card.py
@@ -7,7 +7,7 @@ class Card:
     SUITS:tuple = ("Hearts","Clubs", "Spades", "Diamonds")
     RANKS:tuple = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
 
-    def __init__(self, rank:str, suit:str):
+    def __init__(self, rank:str, suit:str) -> None:
         if rank not in self.RANKS:
             raise ValueError(f"Please input valid rank. Rank must be one of the following {self.RANKS}")
         if suit not in self.SUITS:

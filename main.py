fae32c22d082cecee9bbf1502e6a632005d3478b
William Chen <10979111@gm.nfu.edu.tw>
2022-02-17 22:16:19 +0800

    modify minor format issue

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from poker.card import Card
 def main():
     card1 = Card("2", "Hearts")
     print(card1)
+    _52_cards = cards = Card.generate_52_standard_cards()
+    print(_52_cards)
 
 
 
--- poker/deck.py
+++ poker/deck.py
@@ -1,11 +1,10 @@
 from typing import List
-from typing import List
 from poker.card import Card
 
 class Deck:
     def __init__(self) -> None:
-        self.cards:List = []
+        self._cards:List = []
     
-    def add_cards(self, cards:List[Card]):
-        self.cards.extend(cards)
+    def add_cards(self, cards:List[Card]) -> None:
+        self._cards.extend(cards)
 

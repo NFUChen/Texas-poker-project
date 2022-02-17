f6e01daf9bd9d2a3e48b63121fa5d5f9c5cda3d9
William Chen <10979111@gm.nfu.edu.tw>
2022-02-17 17:36:20 +0800

    Finish Card class in card.py, build out main.py for console

--- poker/card.py
+++ poker/card.py
@@ -1,3 +1,30 @@
+from re import L
+from typing import List 
+
+
 class Card:
-    def __init__(self):
-        pass
+
+    SUITS:tuple = ("Hearts","Clubs", "Spades", "Diamonds")
+    RANKS:tuple = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
+
+    def __init__(self, rank:str, suit:str):
+        if rank not in self.RANKS:
+            raise ValueError(f"Please input valid rank. \
+                               Rank must be one of the following {self.RANKS}")
+        if suit not in self.SUITS:
+            raise ValueError(f"Please input valid suit. \
+                               Suit must be one of the following {self.SUITS}")
+        self.rank = rank
+        self.suit = suit
+
+    
+
+
+
+
+    def __str__(self) -> str:
+        return f"{self.rank} of {self.suit}"
+
+
+    def __repr__(self) -> str:
+        return f"Card ('{self.rank}', '{self.suit}')"
--- tests/test_card.py
+++ tests/test_card.py
@@ -2,13 +2,53 @@ import unittest
 from poker.card import Card
 
 
-class CardTest(unittest.TestCase):
-    def test_setup(self):
-        Card()
-
 
     
 
+class CardTest(unittest.TestCase):
+    def test_has_rank_and_suit(self):
+        test_card = Card(rank= "Queen", suit = "Hearts")
+        self.assertEqual(test_card.rank, "Queen")
+        self.assertEqual(test_card.suit, "Hearts")
+
+    def test_has_string_representation_with_rank_and_suit(self):
+        """
+        implementation of __str__() method in Card class
+        """
+        test_card = Card(rank= "5", suit = "Hearts")
+        self.assertEqual(str(test_card),"5 of Hearts")
+    
+
+    def test_has_technical_representation(self):
+        """
+        implementation of __repr__() method in Card class
+        """
+        test_card = Card(rank= "5", suit = "Hearts")
+        self.assertEqual(repr(test_card),"Card ('5', 'Hearts')")
+
+    def test_has_four_possible_suit_options(self):
+
+        self.assertEqual(len(Card.SUITS),4)
+        self.assertEqual(Card.SUITS, 
+                        ("Hearts","Clubs", "Spades", "Diamonds"))
+
+    def test_has_thirteen_possible_rank_options(self):
+
+        self.assertEqual(len(Card.RANKS),13)
+        self.assertEqual(Card.RANKS, 
+                        ("2", "3", "4", "5", "6", "7", "8", "9", "10", 
+                        "Jack", "Queen", "King", "Ace"))
+
+    def test_card_only_allows_for_valid_rank(self):
+        with self.assertRaises(ValueError):
+            Card(rank = "Two", suit="Hearts")
+
+    def test_card_only_allows_for_valid_suit(self):
+        with self.assertRaises(ValueError):
+            Card(rank = "2", suit="Heart")
+        
+
+    
 
 if __name__ == "__main__":
     unittest.main()
